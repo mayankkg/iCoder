b23613b67c81a9b36d08e26a4dea2119ab5ea400
mksandlus <mayank.sandlus@gmail.com>
2022-03-29 15:32:38 +0530

    First commit

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from blog.models import Post, BlogComment
+
+# Register your models here.
+#admin.site.register(Post)
+#admin.site.register(BlogComment)
+admin.site.register((BlogComment))
+@admin.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js= ('tinyInject.js',)
---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+# it created manually
+
+from django.contrib import admin
+from django.urls import path, include
+from blog import views
+
+urlpatterns = [
+    path('postComment', views.postComment, name="postComment"),
+    path('', views.blogHome, name='blogHome'),
+    path('<str:slug>/', views.blogPost, name='blogPost')
+]
